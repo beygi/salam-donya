68ab6e460db5bd8df922fdaed5225dcea72db132
Mahdy Beygi <mahdy@beygi.org>
2014-08-24 17:09:03 +0430

    fix page structores

--- dev/js/main.js
+++ dev/js/main.js
@@ -30,6 +30,7 @@ $(document).ready(function() {
         //user_tpl = Handlebars.compile($(template).filter('#userTpl').html());
         main_tpl = Handlebars.compile($(template).filter('#mainTpl').html());
         big_tpl = Handlebars.compile($(template).filter('#bigTpl').html());
+        magazine_tpl = Handlebars.compile($(template).filter('#magazineTpl').html());
 
         //parse URL for page name and translation
         lang = document.location.pathname.replace(/^[\/]+/, "").replace(/[\/]$/, "").split('/')[0];
@@ -42,15 +43,17 @@ $(document).ready(function() {
             dataType: "json",
             url: '/lib/data.json'
         }).done(function(data) {
-            remoteData=data;
+            remoteData = data;
             console.log(data);
-            createPage(page,lang);
+            createPage(page, lang);
             //render users
             //for (i = 0; i < data.length; i++) {
             //    if (data[i].fullname === undefined || data[i].fullname === '' || data[i].fullname === null) data[i].fullname = data[i].name;
             //    $('.users').prepend(user_tpl(data[i]));
             //}
-        }).error(function(a,b,c){console.log(a,b,c);});
+        }).error(function(a, b, c) {
+            console.log(a, b, c);
+        });
         //get user data and show them
         /*
         $.ajax({
@@ -73,90 +76,132 @@ $(document).ready(function() {
 });
 
 
-function createPage(name,lang) {
-  var page=findPage(name);
-  page_data={};
-  for(var i=0;i<page.translations.length;i++)
-  {
-    console.log(page.translations[i].code);
-    if (page.translations[i].code===lang) {
-      page_data=page.translations[i].data;
+function createPage(name, lang) {
+    var page = findPage(name);
+    page_data = {};
+    if (page) {
+
+        for (var i = 0; i < page.translations.length; i++) {
+            if (page.translations[i].code === lang) {
+                page_data = page.translations[i].data;
+            }
+        }
+
+        buidData = {};
+        buidData.trans = findTranslation(lang);
+        buidData.topMenus = getTopMenus(lang, name);
+        buidData.lang = lang;
+        buidData.pageData = page_data;
+
+        if (name === 'index') {
+            block = findBlock('aboutMagazine', lang);
+            buidData.first = {
+                title: block.title,
+                text: block.text
+            };
+            buidData.magazine = findLastMagazineData(lang);
+            buidData.magazine.cover = remoteData.magazines[remoteData.magazines.length - 1].cover;
+            buidData.magazine.num = remoteData.magazines[remoteData.magazines.length - 1].num;
+            buidData.magazine.pdf = remoteData.magazines[remoteData.magazines.length - 1].pdf;
+            $('.content').html(main_tpl(buidData));
+        } else if (name === 'magazine') {
+            //get magazine by id
+            var magazineId = document.location.pathname.replace(/^[\/]+/, "").replace(/[\/]$/, "").split('/')[2];
+            var magazine = findMagazineByNum(magazineId, lang);
+            if (magazine) {
+                buidData.magazine = magazine;
+                buidData.pageData = {
+                    title: magazine.title,
+                    text: magazine_tpl(buidData)
+                };
+            } else {
+                buidData.pageData = {
+                    title: "404",
+                    text: "404"
+                };
+            }
+            $('.content').html(big_tpl(buidData));
+        } else {
+            $('.content').html(big_tpl(buidData));
+        }
+        document.title = buidData.pageData.title;
+    } else {
+        buidData = {};
+        buidData.pageData = {
+            title: "404",
+            text: "404"
+        };
+        $('.content').html(big_tpl(buidData));
     }
-  }
-
-  buidData={};
-  buidData.trans = findTranslation(lang);
-  buidData.topMenus=getTopMenus(lang);
-  buidData.lang = lang;
-  buidData.pageData = page_data;
-
-  if (name==='index') {
-    block = findBlock('aboutMagazine',lang);
-    buidData.first = {title :block.title ,text : block.text};
-    buidData.magazine=findLastMagazineData(lang);
-    buidData.magazine.cover=remoteData.magazines[remoteData.magazines.length-1].cover;
-    buidData.magazine.num=remoteData.magazines[remoteData.magazines.length-1].num;
-    buidData.magazine.pdf=remoteData.magazines[remoteData.magazines.length-1].pdf;
-    $('.content').html(main_tpl(buidData));
-  }else
-  {
-    $('.content').html(big_tpl(buidData));
-  }
-  document.title = page_data.title;
 }
 
 function findPage(page) {
-  for (var i=0;i<remoteData.pages.length;i++) {
-    if (page===remoteData.pages[i].url) {
+    for (var i = 0; i < remoteData.pages.length; i++) {
+        if (page === remoteData.pages[i].url) {
 
-      return remoteData.pages[i];
+            return remoteData.pages[i];
+        }
     }
-  }
-  return false;
+    return false;
 }
 
 
-function findBlock(name,lang) {
-  for (var i=0;i<remoteData.staticBlocks.length;i++) {
-    if (name===remoteData.staticBlocks[i].name) {
-      for(var j=0;j<remoteData.staticBlocks[i].translations.length;j++)
-        if (remoteData.staticBlocks[i].translations[j].code===lang)return remoteData.staticBlocks[i].translations[j].data;
-      }
-  }
-  return false;
+function findBlock(name, lang) {
+    for (var i = 0; i < remoteData.staticBlocks.length; i++) {
+        if (name === remoteData.staticBlocks[i].name) {
+            for (var j = 0; j < remoteData.staticBlocks[i].translations.length; j++)
+                if (remoteData.staticBlocks[i].translations[j].code === lang) return remoteData.staticBlocks[i].translations[j].data;
+        }
+    }
+    return false;
 }
 
 function findTranslation(lang) {
-  for (var i=0;i<remoteData.translations.length;i++) {
-        if (remoteData.translations[i].code===lang)return remoteData.translations[i].data;
-  }
-  return false;
+    for (var i = 0; i < remoteData.translations.length; i++) {
+        if (remoteData.translations[i].code === lang) return remoteData.translations[i].data;
+    }
+    return false;
 }
 
 function findLastMagazineData(lang) {
-  for(var j=0;j<remoteData.magazines[remoteData.magazines.length-1].translations.length;j++)
-  {
-        if (remoteData.magazines[remoteData.magazines.length-1].translations[j].code===lang)
-          return remoteData.magazines[remoteData.magazines.length-1].translations[j].data;
-  }
-  return false;
+    for (var j = 0; j < remoteData.magazines[remoteData.magazines.length - 1].translations.length; j++) {
+        if (remoteData.magazines[remoteData.magazines.length - 1].translations[j].code === lang)
+            return remoteData.magazines[remoteData.magazines.length - 1].translations[j].data;
+    }
+    return false;
 }
 
-function getTopMenus(lang)
-{
-  var menus=[];
-  for(var i=0;i<remoteData.TopLinks.length;i++)
-  {
-      for(var j=0;j<remoteData.TopLinks[i].translations.length;j++)
-      {
-        if (remoteData.TopLinks[i].translations[j].code===lang)
-        {
-          var menu=remoteData.TopLinks[i].translations[j].data;
-          menu.url=remoteData.TopLinks[i].url;
-          menu.lang = lang;
-          menus.push(menu);
+function findMagazineByNum(num, lang) {
+    for (var j = 0; j < remoteData.magazines.length; j++) {
+        if (remoteData.magazines[j].num == num)
+            for (var i = 0; i < remoteData.magazines[j].translations.length; i++) {
+                if (remoteData.magazines[j].translations[i].code === lang) {
+                    var magazine = {};
+                    magazine = remoteData.magazines[j].translations[i].data;
+                    magazine.num = remoteData.magazines[j].num;
+                    magazine.cover = remoteData.magazines[j].cover;
+                    magazine.pdf = remoteData.magazines[j].pdf;
+                    return magazine;
+                }
+            }
+    }
+    return false;
+}
+
+function getTopMenus(lang, page) {
+    var menus = [];
+    for (var i = 0; i < remoteData.TopLinks.length; i++) {
+        for (var j = 0; j < remoteData.TopLinks[i].translations.length; j++) {
+            if (remoteData.TopLinks[i].translations[j].code === lang) {
+                var menu = remoteData.TopLinks[i].translations[j].data;
+                menu.url = remoteData.TopLinks[i].url;
+                menu.lang = lang;
+                if (menu.url === page) {
+                    menu.cssClass = 'active';
+                }
+                menus.push(menu);
+            }
         }
-      }
-  }
-  return menus;
+    }
+    return menus;
 }
