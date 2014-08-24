93e9ef59ee0b58308262a16d28c6ca826e8235ce
Mahdy Beygi <mahdy@beygi.org>
2014-08-25 01:36:21 +0430

    add piwik to main.js

--- dev/js/main.js
+++ dev/js/main.js
@@ -120,6 +120,21 @@ function createPage(name, lang) {
             $('.content').html(big_tpl(buidData));
         }
         document.title = buidData.pageData.title;
+
+  //<!-- Piwik -->
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.salam-donya.ir/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', 1]);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+      g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  //<!-- End Piwik Code -->
+
+
     } else {
         buidData = {};
         buidData.pageData = {
