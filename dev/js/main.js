edc12660a753186a0c2263181524d43f57d698d7
Mahdy Beygi <mahdy@beygi.org>
2014-08-30 00:08:14 +0430

    move piwik to main.js , hope it works

--- dev/js/main.js
+++ dev/js/main.js
@@ -230,3 +230,14 @@ function renderTweet(tweet) {
     $('.tweets').css("height", bigger - 60 + 'px');
     $('.tweets').prepend(tweet_tpl(tweet));
 }
+
+
+//PIWIK
+  var _paq = _paq || [];
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.salam-donya.ir/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+    g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
